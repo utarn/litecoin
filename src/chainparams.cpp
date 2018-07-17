5706540e7d40d3f2d3ef73cff2e73b266c6fae28
Utharn Buranasaksee <utarn@yahoo.com>
2018-07-17 19:24:15 +0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -48,8 +48,8 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-    const char* pszTimestamp = "We will have marriage event on 23 September 2018";
-    const CScript genesisOutputScript = CScript() << ParseHex("0422b10732f96efdd1de39c8cc2d4ad234799075729bd890fec017400d603369df300aa234ff14ce863d804ed6465d74c515b9ab8a453c06252cfce89f0c1f037b") << OP_CHECKSIG;
+    const char* pszTimestamp = "NY Times 05/Oct/2011 Steve Jobs, Apple’s Visionary, Dies at 56";
+    const CScript genesisOutputScript = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
 
